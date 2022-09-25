8605d30c890b3f0ce8f03e3f1c628003de68666c
Sanyi <s.truczkai@gmail.com>
2022-09-25 17:26:19 +0200

    implemented drawTriangle(height, borderColor)

--- src/main/java/com/codecool/embroidery/Embroidery.java
+++ src/main/java/com/codecool/embroidery/Embroidery.java
@@ -12,6 +12,8 @@ public class Embroidery {
         System.out.println();
         printShape(drawTriangle(4));
         System.out.println();
+        printShape(drawTriangle(4, 2));
+        System.out.println();
     }
 
     private static int[][] drawRectangle(int width, int height) {
@@ -88,7 +90,22 @@ public class Embroidery {
 
 
     private static int[][] drawTriangle(int height, int borderColor) {
-        return new int[0][0];
+        final int width = height * 2 - 1;
+        final int empty = 0;
+        final int fillColor = 1;
+        int[][] triangle = new int[height][width];
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (j == height - 1 - i || j == height - 1 + i || i == height - 1) {
+                    triangle[i][j] = borderColor;
+                } else if (j > height - 1 - i && j < height - 1 + i) {
+                    triangle[i][j] = fillColor;
+                } else {
+                    triangle[i][j] = empty;
+                }
+            }
+        }
+        return triangle;
     }
 
     private static int[][] drawTriangle(int height, int borderColor, int fillColor) {
